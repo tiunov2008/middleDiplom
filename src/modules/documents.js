5b99945a159516ccf3cff3dc96866c93ac8c289a
Vladimir Tiunov <tiunov2008.va@gmail.com>
2021-07-15 07:47:07 +0300

    add documents

--- src/modules/documents.js
+++ src/modules/documents.js
@@ -8,7 +8,6 @@ const documents = () => {
             }
         }
     });
-    const elements = [...document.querySelector('.img-responsive')];
     document.body.addEventListener('mouseout', e => {
         const target = e.target;
         if (target.closest('.document') !== null) {
@@ -16,6 +15,31 @@ const documents = () => {
             if (targetModal.classList.contains('document')) {
                 target.style.opacity = '0';
             }
+        }
+    });
+    const showModal = src => {
+        const div = document.createElement('div');
+        div.className = 'modal_documents';
+        div.innerHTML = `
+    <img src="${src}" style=" height: 90%;
+    position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%) !important;">`;
+        document.body.append(div);
+        document.querySelector('.overlay').style.display = 'block';
+    };
+    const closeModal = () => {
+        document.querySelector('.modal_documents').remove();
+        document.querySelector('.overlay').style.display = 'none';
+    };
+    document.body.addEventListener('click', e => {
+        const target = e.target;
+        if (target.closest('.document') !== null) {
+            const targetModal = target.closest('.document');
+            if (targetModal.classList.contains('document')) {
+                showModal(target.previousElementSibling.src);
+            }
+        }
+        if (target === document.querySelector('.overlay')) {
+            closeModal();
         }
     });
 };
